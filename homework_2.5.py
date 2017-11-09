cb8cd48cf6fa1e3b41026c5130f1d11cb53bf7e1
Niprichem <vbolshakoff@yandex.ru>
2017-11-10 01:32:59 +0600

    Update code: add existing folder checking, add functions.

--- homework_2.5.py
+++ homework_2.5.py
@@ -3,8 +3,18 @@ import subprocess
 import os
 
 
-if __name__ == '__main__':
+def run_convert(source, img, result):
+    subprocess.run(['convert.exe', os.path.join(source, img), '-resize', '200', os.path.join(result, img)])
+
+
+def main():
     source = 'Source'
     result = 'Result'
+    if not os.path.exists(result):
+        os.mkdir(result)
     for img in os.listdir(source):
-        subprocess.run(['convert.exe', os.path.join(source, img), '-resize', '200', os.path.join(result, img)])
+        run_convert(source, img, result)
+
+
+if __name__ == '__main__':
+    main()
